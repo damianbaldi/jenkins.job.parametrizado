ba1aab7d0787751b798d6ec6d3f63716bd5c585b
Damian Baldi <114240668+damianbaldi@users.noreply.github.com>
2022-11-02 15:19:27 -0300

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job('ejemplo-job-DSL') {
+job('ejemplo2-job-DSL') {
 	description('Job DSL de ejemplo')	
   	scm {
     	git('https://github.com/damianbaldi/jenkins.job.parametrizado.git', 'main') {node ->
